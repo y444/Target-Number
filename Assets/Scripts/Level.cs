987c74332463fae13e5f0d0ec9fe572a8eedaeda
y444 <541m0nk1n9@gmail.com>
2018-10-08 23:07:51 +0300

    start level manager integration

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Level
+{
+    public int rows;
+    public int columns;
+    public int maxValue;
+    public int numberOfTargets;
+}
