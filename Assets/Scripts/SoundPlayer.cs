e9102bbe0b02975f3931fe1c09ecc885ba388658
y444 <541m0nk1n9@gmail.com>
2018-10-15 23:22:55 +0300

    begin sound integration, also forgot to switch branches

---
+++ Assets/Scripts/SoundPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundPlayer : MonoBehaviour {
+
+	public void Play(AudioSource sound)
+    {
+        sound.Play();
+    }
+
+    public void Shuffle(AudioSource[] sound)
+    {
+        int selectedSound = Random.Range(0, sound.Length + 1);
+        sound[selectedSound].Play();
+
+    }
+}
