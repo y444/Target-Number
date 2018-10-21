8b0e73a2f203c40fff2bae3ea9518665e68b0a8c
y444 <541m0nk1n9@gmail.com>
2018-10-21 17:33:10 +0300

    Suggest approach for level generator

--- Assets/Scripts/FieldGenerator.cs
+++ Assets/Scripts/FieldGenerator.cs
@@ -42,5 +42,26 @@ public class FieldGenerator
             }
         }
         //TODO generate everything else
+
+        // VERSION 1: Testing the general approach viability
+
+        // 1. Set all target to random numbers (HOW TO LIMIT?), set global target 
+
+        // 2. Check target cells neighbours, choose a random amount [1, numberOfNeighbours] of neighbours to operate on
+
+        // 3. Subtract random numbers from target until it's zero to the selected amount of neighbours
+
+        // 4. Take one of those neighbour cells and repeat 2.
+
+        // 5. Subtract a random number to the selected amount of neighbours
+
+        // 6. If the cell(s) is left with the value of [minValue, maxValue] and there are other targets to work on
+        // - go work on the next target
+
+        // 7. If the cell is left with the value of [minValue, maxValue] and there are NO other targets
+        // - fill the rest of the field with junk of [minValue, maxValue]
+
+        // 8. When working on second etc. targets if we encounter already filled cells we must not subtract random numbers but 
+        // the number that equals (sum of such neighbours minus current cell)
     }
 }
