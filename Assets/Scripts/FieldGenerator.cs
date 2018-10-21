f4455c5bdbeff967b8d2efeee1a7f3e9b2c6b24e
Aliaksandr Kashkevich <aliaksandr.kashkevich@gmail.com>
2018-10-21 21:28:52 +0300

    remove debug log

--- Assets/Scripts/FieldGenerator.cs
+++ Assets/Scripts/FieldGenerator.cs
@@ -109,7 +109,6 @@ public class FieldGenerator
                 List<Cell> cellsToUse = new List<Cell>(cellToNeighboorsCount.Keys);
 
                 cellsToUse.Sort((key1, key2) => cellToNeighboorsCount[key1] - cellToNeighboorsCount[key2]);
-                Debug.Log("is it zero? " + cellsToUse.Count);
                 Cell currentCell = cellsToUse.First();
                 finalSoultion.Add(currentCell);
                 solutionCells.Remove(currentCell);
